df1f00a5787485d2f8879b6a567da5b3fe73d39c
caleb <caleb@MSI>
2026-03-21 20:27:09 +0000

    Add project files.

---
+++ RndPersonGenerator/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Random Person Generator");
+Console.WriteLine("=======================");
+Console.WriteLine("Press any key to generate a random person...");
+Console.ReadLine();
+Console.Clear();
+
+
